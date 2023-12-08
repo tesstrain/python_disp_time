22934f282865264fb0a354bc0b53cd5770b56dc3
tesstrain <tessolve.training@gmail.com>
2023-12-08 17:58:25 +0530

    Revert "Added Build error"
    This reverts commit 47f7f290959618bf7507b1a9e2fb051776e8afea.

--- java_check1.java
+++ java_check1.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Mars!); //Build error added
+        System.out.println("Hello, Mars!"); 
     }
 }
