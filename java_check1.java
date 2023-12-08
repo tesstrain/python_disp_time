47761b6bc622919afabc96fff666ba4c771719e0
Siva Chidambaram <siva52.shri238@gmail.com>
2023-12-08 18:12:40 +0530

    Build error changed

--- java_check1.java
+++ java_check1.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Saturn!, Hakeem); //BUILD ERROR
+        System.out.println("Hello, Saturn!, Siva); //BUILD ERROR
     }
 }
